f0d59d7296cb8e980f7e511fb067250f1dd7299d
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2021-03-07 19:11:03 +0330

    setup ApiService

--- app/src/main/java/com/mazibahrami/example/ui/MainActivity.kt
+++ app/src/main/java/com/mazibahrami/example/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mazibahrami.example
+package com.mazibahrami.example.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mazibahrami.example.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
