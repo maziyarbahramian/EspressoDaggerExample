a6465797a85bb53e6e86320a9d8907aef35d5358
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2021-03-08 15:31:58 +0330

    create FakeGlideRequestManager

---
+++ app/src/androidTest/java/com/mazibahrami/example/util/FakeGlideRequestManager.kt
@@ -0,0 +1,14 @@
+package com.mazibahrami.example.util
+
+import android.widget.ImageView
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FakeGlideRequestManager
+@Inject
+constructor() : GlideManager {
+    override fun setImage(imageUrl: String, imageView: ImageView) {
+        // does nothing
+    }
+}
